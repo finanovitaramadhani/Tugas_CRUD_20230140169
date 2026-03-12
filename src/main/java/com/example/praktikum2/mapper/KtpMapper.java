174aa2f7940b5ecc6c9f3aa185543436d77f4ee1
finanovita <finanovita479@gmail.com>
2026-03-13 00:34:46 +0700

    menambahkan mapper MapStruct untuk konversi entity Ktp dan KtpDto

---
+++ src/main/java/com/example/praktikum2/mapper/KtpMapper.java
@@ -0,0 +1,17 @@
+package com.example.praktikum2.mapper;
+
+import com.example.praktikum2.model.dto.KtpDto;
+import com.example.praktikum2.model.entity.Ktp;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface KtpMapper {
+
+    KtpMapper MAPPER = Mappers.getMapper(KtpMapper.class);
+
+    KtpDto toDto(Ktp ktp);
+
+    Ktp toEntity(KtpDto dto);
+
+}
